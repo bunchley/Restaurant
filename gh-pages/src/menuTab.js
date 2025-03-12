50da6194cc91b4cd64603419b21a9988c1ee3eab
Ashley Bunch <bunchley007@gmail.com>
2025-03-12 16:59:12 -0600

    Merge commit 'e606d3dc9d3ba4ad09d5c9b7b258281551e45afd' as 'gh-pages'

---
+++ gh-pages/src/menuTab.js
@@ -0,0 +1,28 @@
+"use strict";
+const menuTab = function () {
+  const contentBlock = document.getElementById("content");
+
+  let menuBlock = document.createElement("div");
+  menuBlock.className = "menu tabContent";
+  contentBlock.appendChild(menuBlock);
+
+  let container = document.createElement("div");
+  container.className = "menuContainer";
+  menuBlock.appendChild(container);
+
+  let title = document.createElement("h2");
+  title.className = "subHeadText";
+  // title.innerHTML = "Arrogant Chicken Grub";
+  menuBlock.appendChild(title);
+
+  let chickenMenu = document.createElement("img");
+  chickenMenu.className = "chicken menu";
+  chickenMenu.src = "../img/ChickenMenu.jpg";
+  menuBlock.appendChild(chickenMenu);
+
+  let sideMenu = document.createElement("img");
+  sideMenu.className = "side menu";
+  sideMenu.src = "../img/SideMenu.jpg";
+  menuBlock.appendChild(sideMenu);
+};
+module.exports = { menuTab: menuTab };
